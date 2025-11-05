0a1bb977fc25f3b661fc9baad5cff9b1c5b4f178
EmmyCodes234 <emmycodes234@gmail.com>
2025-11-05 07:09:10 +0100

    fix ratings page 7

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './src/animations.css';
+import './src/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 export default {
-  plugins: [
-    // If you're not using Tailwind CSS, you can leave this empty
-    // Or if you want to use Tailwind, make sure to install it properly
-  ]
+  plugins: {
+    '@tailwindcss/postcss': {}
+  }
 }
