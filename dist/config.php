74377575e7918f9c13024d34aeaa3563af7e054c
EmmyCodes234 <emmycodes234@gmail.com>
2026-07-02 13:47:46 +0100

    chore: add .cpanel.yml and built dist for deployment

---
+++ dist/config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'CEREBRAS_API_KEY' => 'csk-k89p2v35jem3dkxrjc2y3ykkmwmfpj9vw3crex5vw36frvr8',
+    'SUPABASE_URL' => 'https://dbuegnixoxtagpeatgjl.supabase.co',
+    'SUPABASE_ANON_KEY' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRidWVnbml4b3h0YWdwZWF0Z2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMjE4NjcsImV4cCI6MjA3Mzc5Nzg2N30.F2ZQdzguuoXWRZnSCjDSwJiOdFX45apdNx2UYPxPnsk'
+];
